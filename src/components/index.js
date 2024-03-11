7a78d87a0f0c939db2b3d7a941a9a540acde2730
JohanLiebert <nguyenloc24022005@gmail.com>
2024-03-11 13:54:17 +0700

    feat: create SingleProduct Page

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,9 @@
 export { default as FormInput } from "./FormInput";
 export { default as SubmitBtn } from "./SubmitBtn";
 export { default as Header } from "./Header";
-export { default as Navbar} from './Navbar';
-export { default as NavLinks} from './NavLinks';
-export { default as ErrorElement} from './ErrorElement';
-export { default as TitleSection} from './TitleSection';
-export { default as FeaturedProductItems} from './FeaturedProductItems';
-export { default as FeaturedProductsSection} from './FeaturedProductsSection';
-export { default as SingleProductItem} from './SingleProductItem';
-
+export { default as Navbar } from "./Navbar";
+export { default as NavLinks } from "./NavLinks";
+export { default as ErrorElement } from "./ErrorElement";
+export { default as TitleSection } from "./TitleSection";
+export { default as FeaturedProductItems } from "./FeaturedProductItems";
+export { default as FeaturedProductsSection } from "./FeaturedProductsSection";
--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -1,6 +1,8 @@
-import { customFetch } from "../utils";
+import { customFetch, formatPrice } from "../utils";
 
-import { SingleProductItem } from "../components";
+import { useLoaderData, Link } from "react-router-dom";
+
+import { useState } from "react";
 
 export const loader = async ({ params }) => {
     const res = await customFetch(`/products/${params.id}`);
@@ -11,10 +13,97 @@ export const loader = async ({ params }) => {
 };
 
 function SingleProduct() {
+    const { singleProduct } = useLoaderData();
+
+    const { image, title, description, price, colors, company } =
+        singleProduct.attributes;
+
+    const dollarAmount = formatPrice(price);
+
+    const [amount, setAmount] = useState(1);
+    const [chosenColor, setChosenColor] = useState(colors[0]);
+
+    const handleAmount = (e) => {
+        setAmount(parseInt(e.target.value));
+    };
+
     return (
-        <>
-            <SingleProductItem />
-        </>
+        <section>
+            <div className="text-md breadcrumbs">
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/products">Products</Link>
+                    </li>
+                </ul>
+            </div>
+
+            {/* Product Content */}
+            <div className="mt-6 grid gap-y-8 lg:grid-cols-2  lg:gap-x-16">
+                <img
+                    src={image}
+                    alt={title}
+                    className="w-96 h-96 object-cover rounded-lg lg:w-full"
+                />
+                <div>
+                    <h1 className="capitalize text-3xl font-bold">{title}</h1>
+                    <h4 className="text-xl text-neutral-content font-bold mt-2">
+                        {company}
+                    </h4>
+                    <p className="mt-3 text-xl">{dollarAmount}</p>
+                    <p className="mt-6 leading-8">{description}</p>
+
+                    {/* Colors */}
+                    <div className="mt-6">
+                        <h4 className="text-md font-medium tracking-wider capitalize">
+                            colors
+                        </h4>
+                        <div className="mt-2">
+                            {colors.map((color, index) => (
+                                <button
+                                    key={index}
+                                    type="button"
+                                    style={{ background: color }}
+                                    onClick={() => setChosenColor(color)}
+                                    className={`badge w-6 h-6 mr-2 ${
+                                        color === chosenColor &&
+                                        "border-2 border-secondary"
+                                    }`}
+                                ></button>
+                            ))}
+                        </div>
+                    </div>
+                    {/* Select amount */}
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <h4 className="text-md font-medium tracking-wider capitalize">
+                                Amount
+                            </h4>
+                        </label>
+                        <select
+                            className="select select-secondary select-bordered select-md"
+                            value={amount}
+                            onChange={handleAmount}
+                        >
+                            <option value={1}>1</option>
+                            <option value={2}>2</option>
+                            <option value={3}>3</option>
+                        </select>
+                    </div>
+                    {/* Buttons */}
+                    <div className="mt-10 ">
+                        <button
+                            className="btn btn-secondary btn-md"
+                            onClick={() => console.log("Added to bag!")}
+                        >
+                            ADD TO BAG
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 }
 
